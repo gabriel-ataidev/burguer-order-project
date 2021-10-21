6e0249fbef19883c930dfd34cb62b39c6f8c558b
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-21 12:06:05 -0300

    setting up footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,40 @@
 <template>
-  <p>Footer</p>
+  <footer id="footer">
+    <img src="/img/instagram-icon.png" />
+    <p>MAKE YOUR BURGUER &copy; 2021</p>
+    <img src="/img/whatsapp-icon.png" />
+  </footer>
 </template>
 
 <script>
 export default {
-    name: "Footer",
-    
-}
+  name: "Footer",
+};
 </script>
 
-<style>
-
+<style lang="less">
+#footer{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 70px;
+  background: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  width: 100%;
+  p{
+    max-width: fit-content;
+    margin: 0 50px;
+    font-size: 18px;
+  }
+  img{
+    height: 30px;
+    width: auto;
+    cursor: pointer;
+    transition: all .4s ease-in-out;
+    &:hover{
+      opacity: 0.7;
+    }
+  }
+}
 </style>
