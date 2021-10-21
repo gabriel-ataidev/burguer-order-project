ccbc2a06ecd6c3a88dbae41f9079b5adab92366c
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-21 12:05:55 -0300

    setting up nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,61 @@
 <template>
   <div id="nav">
-    <router-link to="/">
-        <img src="" alt="">
+    <router-link to="/" id="logo-url">
+      <span id="title">MYB</span>
+      <img id="logo" :src="logo" :alt="alt" />
     </router-link>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/pedidos">Pedidos</router-link>
+    <div class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/pedidos">Pedidos</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Navbar',
-}
+  name: "Navbar",
+  props: ["logo", "alt"],
+};
 </script>
 
 <style scoped lang="less">
-
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+#nav {
+  background: #222;
+  border-bottom: 4px solid #111;
+  padding: 10px 90px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  a {
+    color: #fcba03;
+    font-size: 25px;
+    text-decoration: none;
+    margin: 0 12px;
+    transition: all 0.5s ease-in-out;
+    &:hover {
+      color: #fff;
+    }
+  }
+  #title{
+    font-size: 45px;
+    color: #fff;
+  }
+  #logo-url {
+    margin: 5px 40px 0 0;
+    #logo {
+      height: 40px;
+      width: auto;
+    }
+  }
+  .links{
+    display: flex;
+    gap: 30px;
+    align-items: center;
+  }
+}
 </style>
