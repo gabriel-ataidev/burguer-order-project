4afae4d70bfb3fe226a1d22eda3c8553cab42a6f
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-22 20:03:06 -0300

    updating order status

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -20,8 +20,8 @@
           </ul>
         </div>
         <div>
-          <select name="status" class="status">
-            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo"> {{s.tipo}} </option>
+          <select name="status" class="status" @change="updateBurger($event, burger.id)">
+            <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo"> {{s.tipo}} </option>
           </select>
           <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
@@ -60,6 +60,18 @@ export default {
       });
       const res = await req.json();
       this.getOrders();
+    },
+    async updateBurger(event, id) {
+      const option = event.target.value;
+      const dataJson = JSON.stringify({ status: option});
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: dataJson
+      });
+      const res = await req.json();
+      console.log(res);
+
     }
   },
   mounted() {
